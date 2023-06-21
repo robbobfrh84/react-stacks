06ba6d9339a5f986481fd2bbd8c67984198d8790
robbobfrh84 <bobmain49@gmail.com>
2023-06-21 14:30:01 -0700

    Updates

---
+++ kts-webapp/precache-manifest.e947478682fd8c6a857ca214eff21d19.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b134795f918c98487fa67f03b05e63e",
+    "url": "/react-stacks/kts-webapp/index.html"
+  },
+  {
+    "revision": "01c79523058a461f1bbf",
+    "url": "/react-stacks/kts-webapp/static/css/main.b39a037c.chunk.css"
+  },
+  {
+    "revision": "4a52144d21769daef771",
+    "url": "/react-stacks/kts-webapp/static/js/2.8934c7ec.chunk.js"
+  },
+  {
+    "revision": "037b147ed273385325e500ff12af141d",
+    "url": "/react-stacks/kts-webapp/static/js/2.8934c7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c79523058a461f1bbf",
+    "url": "/react-stacks/kts-webapp/static/js/main.f8bbbb7a.chunk.js"
+  },
+  {
+    "revision": "3c06f61fc4394f78bf76",
+    "url": "/react-stacks/kts-webapp/static/js/runtime-main.c6043e40.js"
+  },
+  {
+    "revision": "0d5114241fb2ce95c3217b8445b1b10a",
+    "url": "/react-stacks/kts-webapp/static/media/edit_icon.0d511424.svg"
+  },
+  {
+    "revision": "288cc37eb5f7aa75b85408664d635580",
+    "url": "/react-stacks/kts-webapp/static/media/icon_check_green.288cc37e.svg"
+  },
+  {
+    "revision": "906a3591707225a934b618e50ac4bc47",
+    "url": "/react-stacks/kts-webapp/static/media/icon_exit_modal.906a3591.svg"
+  },
+  {
+    "revision": "fcac82290f12b5c3f8bb93c593ec1f0a",
+    "url": "/react-stacks/kts-webapp/static/media/icon_exit_modal_black.fcac8229.svg"
+  },
+  {
+    "revision": "562ce5e177487992b28c2169da5508cf",
+    "url": "/react-stacks/kts-webapp/static/media/icon_exit_modal_red.562ce5e1.svg"
+  },
+  {
+    "revision": "ec8e459a504df298e69ba7c7486749f3",
+    "url": "/react-stacks/kts-webapp/static/media/icon_loading.ec8e459a.svg"
+  },
+  {
+    "revision": "be660857e32d56e2aa2458dce8e3f944",
+    "url": "/react-stacks/kts-webapp/static/media/icon_menu.be660857.svg"
+  },
+  {
+    "revision": "6431d77adde1307c5d7ba935a912f6bd",
+    "url": "/react-stacks/kts-webapp/static/media/icon_menu_close.6431d77a.svg"
+  },
+  {
+    "revision": "3553549d4e09e28c5d52e4d8dfd27d22",
+    "url": "/react-stacks/kts-webapp/static/media/paginate_arrow.3553549d.svg"
+  },
+  {
+    "revision": "f48696c01bbe71454f3a41935ce63b2f",
+    "url": "/react-stacks/kts-webapp/static/media/resume_button.f48696c0.svg"
+  },
+  {
+    "revision": "f48696c01bbe71454f3a41935ce63b2f",
+    "url": "/react-stacks/kts-webapp/static/media/start_button.f48696c0.svg"
+  },
+  {
+    "revision": "51aa020b76fc71090c4c31949c1283ef",
+    "url": "/react-stacks/kts-webapp/static/media/title_header_dark.51aa020b.png"
+  },
+  {
+    "revision": "0dc8ca49b13584a72bdd463b934e9fac",
+    "url": "/react-stacks/kts-webapp/static/media/title_header_light.0dc8ca49.png"
+  }
+]);
